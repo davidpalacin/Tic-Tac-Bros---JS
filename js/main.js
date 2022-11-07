b6d8602e6de4b9940adfd720002dbf4296b692de
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-11-07 20:02:34 +0100

    starting with game logic. Trying to create turns and players

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-let field = document.getElementsByClassName("tictac-grid-row-field");
+let tablero = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+];
+let players = ["Player 1", "Player 2"];
+let turno = players[0];
+let infoTurno = document.createTextNode(players[0]);
+document.getElementById("turno").appendChild(infoTurno);
 
-for (let i = 0; i < field.length; i++) {
-    field[i].id = "field"+i;
-    field[i].setAttribute("onclick", "addPiece(event)");
+// AÑADIR EL EVENTO A TODAS LAS CASILLAS
+let fields = document.getElementsByClassName("tictac-grid-row-field");
+for (let i = 0; i < fields.length; i++) {
+    fields[i].id = "field"+i;
+    fields[i].setAttribute("onclick", "addPiece(event)");
 }
 
 // AÑADIR FICHA SEGUN EL TURNO
 function addPiece(event){
-    if(isFieldEmpty(event)){
+    if(isFieldEmpty(event)){    
         let pieceX = document.createTextNode("X");
         document.getElementById(event.target.id).appendChild(pieceX);
+        changeTurn(turno);
     }
 }
 function addPieceO(event) {
@@ -19,16 +30,19 @@ function addPieceO(event) {
   }
 }
 
-// COMPRUEBA Y DEVUEVE BOOLEANO SEGUN SI LA CASILLA ESTÁ VACIA
+// COMPRUEBA Y DEVUELVE BOOLEANO SEGUN SI LA CASILLA ESTÁ VACIA
 function isFieldEmpty(event){
     if (document.getElementById(event.target.id).hasChildNodes()){
         return false;
     }
     return true;
 }
-
-
-
-// field.addEventListener("click", (event) => {
-//   field.textContent = `Click count: ${event.detail}`;
-// });
+function changeTurn(turno){
+    let updatedTurn = turno;
+    if (turno === players[0]) {
+      updatedTurn = document.createTextNode(players[1]);
+    } else {
+      updatedTurn = document.createTextNode(players[0]);
+    }
+    document.getElementById("turno").appendChild(updatedTurn);
+}
