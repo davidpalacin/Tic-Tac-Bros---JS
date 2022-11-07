225954f9cce95b2b016904925b3dcec893056c5a
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-11-07 13:53:11 +0100

    COMENTAR

--- js/main.js
+++ js/main.js
@@ -5,13 +5,21 @@ for (let i = 0; i < field.length; i++) {
     field[i].setAttribute("onclick", "addPiece(event)");
 }
 
+// AÑADIR FICHA SEGUN EL TURNO
 function addPiece(event){
     if(isFieldEmpty(event)){
         let pieceX = document.createTextNode("X");
         document.getElementById(event.target.id).appendChild(pieceX);
     }
 }
+function addPieceO(event) {
+  if (isFieldEmpty(event)) {
+    let pieceO = document.createTextNode("O");
+    document.getElementById(event.target.id).appendChild(pieceO);
+  }
+}
 
+// COMPRUEBA Y DEVUEVE BOOLEANO SEGUN SI LA CASILLA ESTÁ VACIA
 function isFieldEmpty(event){
     if (document.getElementById(event.target.id).hasChildNodes()){
         return false;
