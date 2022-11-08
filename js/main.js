2d7d89b326c3abdfd8219d1fe5a94ee916a605da
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-11-08 09:57:16 +0100

    improving change turn

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let tablero = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
 ];
 let players = ["Player 1", "Player 2"];
 let turno = players[0];
@@ -11,38 +11,46 @@ document.getElementById("turno").appendChild(infoTurno);
 // AÑADIR EL EVENTO A TODAS LAS CASILLAS
 let fields = document.getElementsByClassName("tictac-grid-row-field");
 for (let i = 0; i < fields.length; i++) {
-    fields[i].id = "field"+i;
-    fields[i].setAttribute("onclick", "addPiece(event)");
+  fields[i].id = "field" + i;
+  fields[i].setAttribute("onclick", "addPiece(event)");
 }
 
 // AÑADIR FICHA SEGUN EL TURNO
-function addPiece(event){
-    if(isFieldEmpty(event)){    
-        let pieceX = document.createTextNode("X");
-        document.getElementById(event.target.id).appendChild(pieceX);
-        changeTurn(turno);
-    }
-}
-function addPieceO(event) {
+function addPiece(event) {
   if (isFieldEmpty(event)) {
-    let pieceO = document.createTextNode("O");
-    document.getElementById(event.target.id).appendChild(pieceO);
+    if(turno == players[0]){
+      console.log("Turno jugador 1");
+      let pieceX = document.createTextNode("X");
+      document.getElementById(event.target.id).appendChild(pieceX);
+    }
+    // else{
+    //   let pieceO = document.createTextNode("O");
+    //   document.getElementById(event.target.id).appendChild(pieceO);
+    // }
+    changeTurn(turno);
   }
 }
 
 // COMPRUEBA Y DEVUELVE BOOLEANO SEGUN SI LA CASILLA ESTÁ VACIA
-function isFieldEmpty(event){
-    if (document.getElementById(event.target.id).hasChildNodes()){
-        return false;
-    }
-    return true;
+function isFieldEmpty(event) {
+  if (document.getElementById(event.target.id).hasChildNodes()) {
+    return false;
+  }
+  return true;
 }
-function changeTurn(turno){
-    let updatedTurn = turno;
-    if (turno === players[0]) {
-      updatedTurn = document.createTextNode(players[1]);
-    } else {
-      updatedTurn = document.createTextNode(players[0]);
-    }
-    document.getElementById("turno").appendChild(updatedTurn);
+function changeTurn(turno) {
+  console.log("Se intenta cambiar el turno de " + turno);
+  if (turno === players[0]) {
+    console.log("confirmado el turno es de " + turno);
+    let updatedTurn = document.createTextNode(players[1]);
+    console.log("El turno nuevo seria de " + players[1]);
+    console.log(document.getElementById("turno"));
+    // let element = document.getElementById("turno").children[0];
+    // element.replaceChild(updatedTurn, element.childNodes[0]);
+  }
+  //  else {
+  //   const updatedTurn = document.createTextNode(players[0]);
+  //   const element = document.getElementById("turno").children[0];
+  //   element.replaceChild(updatedTurn, element.childNodes[0]);
+  // }
 }
